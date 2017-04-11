de552084a3aab4bc2326a47ffdfafb40ecf06d70
Aaron Dean <arasdean@gmail.com>
2017-04-11 19:34:19 +0000

    functioning js for sending info to Stripe

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -11,12 +11,36 @@ $(document).on('turbolinks:load', function(){
         // prevent default sub behavior.
         
         event.preventDefault();
+        submitBtn.val("Processing").prop('disbaled', true);
         // collect cc field
         
         var ccNum = $('#card_number').val(),
         cvcNum = $('#card_code').val(), 
         expMonth = $('#date_month').val(),
         expYear = $('#card_year').val();
+        
+        //Use Stripe.js library to check for card errors 
+        // error handling
+        var error = false 
+        //Validate card # 
+        
+        if(!Stripe.card.validateCardNumber(ccNum)){
+            error= true;
+        }
+        if(!Stripe.card.validateCVC(cvcNum)){
+            error= true;
+        }
+        if(!Stripe.card.validateExpiry(expMonth, expYear)){
+            error= true;
+        }
+        if(error) {
+            //If there is an error, don't send to stripe
+            alert('Credit card number is invalid') 
+            submitBtn.prop('disabled',false).val("Sign Up") 
+        }
+        
+        else {
+        
         
         //send card info to stripe 
         Stripe.createToken({
@@ -25,7 +49,8 @@ $(document).on('turbolinks:load', function(){
             exp_month: expMonth,
             exp_year: expYear},
         stripeResponseHandler);
-        //Stripe returns card token
+        return false 
+    }
         
         
     });
@@ -33,7 +58,18 @@ $(document).on('turbolinks:load', function(){
     
  
     //Stripe returns card token 
-    //inject as hidden field
+    function stripeResponseHandler(status, response) {
+        // get token from response 
+        var token = response.id;
+        
+        //inject as hidden field
+        theForm.append( $('<input type "hidden" name="user[stripe_card_token]"> ').val(token));
+    
+        //Submit Form 
+        theForm.get(0).submit; 
+        
+    } 
+    
     
 }); 
 
