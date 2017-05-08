0003ea696f16c809cfa666ce2245815f53eea41b
Aaron Dean <arasdean@gmail.com>
2017-05-08 19:06:29 +0000

    created community page and styled it

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,39 +1,46 @@
 class ProfilesController < ApplicationController 
-    # GET to /users/:user_id/profile/new
-    def new 
-        #Render blank profile details form 
-        @profile = Profile.new 
-    end 
-    def create 
-      @user = User.find( params[:user_id])
-      @profile = @user.build_profile( profile_params) 
-      if @profile.save 
-        flash[:success] = "Profile Updated" 
-        redirect_to user_path(params[:user_id])
-      else
-        render action: :new 
-      end
+  before_action :authenticate_user! 
+  before_action :only_current_user
+  
+  # GET to /users/:user_id/profile/new
+  def new 
+      #Render blank profile details form 
+      @profile = Profile.new 
+  end 
+  def create 
+    @user = User.find( params[:user_id])
+    @profile = @user.build_profile( profile_params) 
+    if @profile.save 
+      flash[:success] = "Profile Updated" 
+      redirect_to user_path(params[:user_id])
+    else
+      render action: :new 
     end
-    
-    def edit
-      #this is for get requests made to /users/:user_id/profile/edit
-      @user = User.find( params[:user_id]) 
-      @profile = @user.profile
+  end
+  
+  def edit
+    #this is for get requests made to /users/:user_id/profile/edit
+    @user = User.find( params[:user_id]) 
+    @profile = @user.profile
+  end 
+  # PUT req to /isers/user_id/profile
+  def update
+    @user = User.find(params[:user_id]) 
+    @profile = @user.profile
+    if @profile.update_attributes(profile_params)
+      flash[:success] = "Profile Updated" 
+      redirect_to root_url
+    else
+      render action: :edit 
     end 
-    # PUT req to /isers/user_id/profile
-    def update
-      @user = User.find(params[:user_id]) 
-      @profile = @user.profile
-      if @profile.update_attributes(profile_params)
-        flash[:success] = "Profile Updated" 
-        redirect_to root
-      else
-        render action: :edit 
-      end 
+  end
+  
+  private
+    def profile_params
+      params.require(:profile).permit(:first_name, :last_name, :avatar, :job_title, :phone_number, :contact_email, :description)
+    end  
+    def only_current_user
+    @user = User.find(params[:user_id]) 
+    redirect_to(root_url) unless @user == current_user 
     end
-    
-    private
-      def profile_params
-        params.require(:profile).permit(:first_name, :last_name, :avatar, :job_title, :phone_number, :contact_email, :description)
-      end  
 end 
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,11 @@
 class UsersController < ApplicationController 
   # GET to /users/id
+  before_action :authenticate_user! 
+  def index
+    @users = User.includes(:profile)
+  end 
   def show 
     @user = User.find( params[:id])
   end
+  
 end
